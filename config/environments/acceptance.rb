9791beebc425e84dea2ebc7407fc2ea05f53645f
Andrew Hao <andrewhao@gmail.com>
2015-01-30 23:32:55 -0800

    Update references to new repo name

--- config/environments/acceptance.rb
+++ config/environments/acceptance.rb
@@ -4,5 +4,5 @@ require Rails.root.join('config/environments/production')
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb and config/production.rb.
 
-  config.action_mailer.default_url_options = { host: 'stressor-core-acceptance.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'velocitas-core-acceptance.herokuapp.com' }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,5 +74,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'stressor-core.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'velocitas-core.herokuapp.com' }
 end
