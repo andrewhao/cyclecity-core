0baa3b41e039b1f96bb7a30c4f898aa58c541def
Andrew Hao <andrewhao@gmail.com>
2015-02-02 06:14:41 -0800

    Shift up the adapter override

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,33 @@ require File.expand_path('../boot', __FILE__)
 
 # require 'rails/all'
 require 'rails'
+
+# From https://devcenter.heroku.com/articles/postgis, we need to
+# reset Rails' default adapter to PostGIS.
+# in config/application.rb, before the application loads
+class PostGisOverrideRailtie < Rails::Railtie
+  initializer "active_record.initialize_database.override" do |app|
+
+    ActiveSupport.on_load(:active_record) do
+      Rails.logger.info "Initializing PostGIS AR adapter override for prod environments."
+      if url = ENV['DATABASE_URL']
+        ActiveRecord::Base.connection_pool.disconnect!
+        parsed_url = URI.parse(url)
+        config =  {
+          adapter:             'postgis',
+          host:                parsed_url.host,
+          encoding:            'unicode',
+          database:            parsed_url.path.split("/")[-1],
+          port:                parsed_url.port,
+          username:            parsed_url.user,
+          password:            parsed_url.password
+        }
+        establish_connection(config)
+      end
+    end
+  end
+end
+
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_view/railtie'
@@ -43,28 +70,3 @@ module VelocitasCore
   end
 end
 
-# From https://devcenter.heroku.com/articles/postgis, we need to
-# reset Rails' default adapter to PostGIS.
-# in config/application.rb, before the application loads
-class PostGisOverrideRailtie < Rails::Railtie
-  initializer "active_record.initialize_database.override" do |app|
-
-    ActiveSupport.on_load(:active_record) do
-      Rails.logger.info "Initializing PostGIS AR adapter override for prod environments."
-      if url = ENV['DATABASE_URL']
-        ActiveRecord::Base.connection_pool.disconnect!
-        parsed_url = URI.parse(url)
-        config =  {
-          adapter:             'postgis',
-          host:                parsed_url.host,
-          encoding:            'unicode',
-          database:            parsed_url.path.split("/")[-1],
-          port:                parsed_url.port,
-          username:            parsed_url.user,
-          password:            parsed_url.password
-        }
-        establish_connection(config)
-      end
-    end
-  end
-end
