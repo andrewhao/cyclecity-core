450da61b283aad4c6fc98dee0e5c714798a762f2
Andrew Hao <andrewhao@gmail.com>
2015-02-09 00:42:09 -0800

    Add logging

--- app/models/track_point.rb
+++ app/models/track_point.rb
@@ -7,7 +7,10 @@ class TrackPoint < ActiveRecord::Base
   set_rgeo_factory_for_column(:coordinate, RGeo::Geographic.spherical_factory(:srid => 4326))
 
   def self.create_from_gpx_track_point(tp, parent_track: parent_track)
+    Rails.logger.info "TrackPoint from GPX is: lon-#{tp.lon} and lat-#{tp.lat}."
     coordinate = point_factory.point(tp.lon, tp.lat)
+    Rails.logger.info "point_factory is: #{point_factory.inspect}"
+    Rails.logger.info "coordinate is: #{coordinate.inspect}"
     create elevation: tp.elevation, time: tp.time, track: parent_track, coordinate: coordinate
   end
 
