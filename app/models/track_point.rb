39a778e7e62d8406ed3702f6c767c62639c17797
Andrew Hao <andrewhao@gmail.com>
2015-04-07 23:52:36 -0700

    Remove point factory classes.

--- app/models/track_point.rb
+++ app/models/track_point.rb
@@ -3,9 +3,6 @@
 class TrackPoint < ActiveRecord::Base
   belongs_to :track
 
-  # But use a geographic implementation for the :coordinate column.
-  set_rgeo_factory_for_column(:coordinate, RGeo::Geographic.spherical_factory(:srid => 4326))
-
   def self.create_from_gpx_track_point(tp, parent_track: parent_track)
     Rails.logger.info "TrackPoint from GPX is: lon-#{tp.lon} and lat-#{tp.lat}."
     coordinate = point_factory.point(tp.lon, tp.lat)
