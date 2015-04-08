42da446e65e21ec214ca5926d0dfa368cfeaa41a
Andrew Hao <andrewhao@gmail.com>
2015-04-08 00:36:58 -0700

    Testing other fix

--- app/models/track_point.rb
+++ app/models/track_point.rb
@@ -4,14 +4,14 @@ class TrackPoint < ActiveRecord::Base
   belongs_to :track
 
   # But use a geographic implementation for the :coordinate column.
-  set_rgeo_factory_for_column(:coordinate, RGeo::Geographic.spherical_factory(:srid => 4326))
+  set_rgeo_factory_for_column(:coordinate, point_factory)
 
   def self.create_from_gpx_track_point(tp, parent_track: parent_track)
     Rails.logger.info "TrackPoint from GPX is: lon-#{tp.lon} and lat-#{tp.lat}."
     coordinate = point_factory.point(tp.lon, tp.lat)
     Rails.logger.info "point_factory is: #{point_factory.inspect}"
     Rails.logger.info "coordinate is: #{coordinate.inspect}"
-    create elevation: tp.elevation, time: tp.time, track: parent_track, coordinate: coordinate
+    create coordinate: coordinate
   end
 
   def latitude
