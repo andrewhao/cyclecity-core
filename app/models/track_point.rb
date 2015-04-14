cb4434f4a82cefb13ede40bdcf0e4a7fb70a68c8
Andrew Hao <andrewhao@gmail.com>
2015-04-13 21:08:13 -0700

    Remove logging

--- app/models/track_point.rb
+++ app/models/track_point.rb
@@ -4,10 +4,7 @@ class TrackPoint < ActiveRecord::Base
   belongs_to :track
 
   def self.create_from_gpx_track_point(tp, parent_track: parent_track)
-    Rails.logger.info "TrackPoint from GPX is: lon-#{tp.lon} and lat-#{tp.lat}."
     coordinate = point_factory.point(tp.lon, tp.lat)
-    Rails.logger.info "point_factory is: #{point_factory.inspect}"
-    Rails.logger.info "coordinate is: #{coordinate.inspect}"
     create elevation: tp.elevation, time: tp.time, track: parent_track, coordinate: coordinate
   end
 
