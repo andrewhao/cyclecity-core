b06bf7026a349b04be8ca24d63bcff12e097abb7
Andrew Hao <andrew@carbonfive.com>
2016-06-16 17:41:01 -0700

    Upgrade postgis adapter, plus remove column dumps from schema.

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'puma'
 gem 'rack-canonical-host'
 gem 'rails', '~> 4.2.0'
 gem 'pg'
-gem 'activerecord-postgis-adapter', "3.0.0.beta2"
+gem 'activerecord-postgis-adapter'
 gem 'grape'
 gem "gpx", require: "gpx"
 gem "pry-byebug"
--- db/schema.rb
+++ db/schema.rb
@@ -90,64 +90,6 @@ ActiveRecord::Schema.define(version: 20160526064302) do
   add_foreign_key "commuting_stop_reports", "commuting_commutes"
   add_foreign_key "track_analytics", "tracks"
 
-  create_view :geography_columns,  sql_definition: <<-SQL
-      SELECT current_database() AS f_table_catalog,
-      n.nspname AS f_table_schema,
-      c.relname AS f_table_name,
-      a.attname AS f_geography_column,
-      postgis_typmod_dims(a.atttypmod) AS coord_dimension,
-      postgis_typmod_srid(a.atttypmod) AS srid,
-      postgis_typmod_type(a.atttypmod) AS type
-     FROM pg_class c,
-      pg_attribute a,
-      pg_type t,
-      pg_namespace n
-    WHERE ((t.typname = 'geography'::name) AND (a.attisdropped = false) AND (a.atttypid = t.oid) AND (a.attrelid = c.oid) AND (c.relnamespace = n.oid) AND (NOT pg_is_other_temp_schema(c.relnamespace)) AND has_table_privilege(c.oid, 'SELECT'::text));
-  SQL
-
-  create_view :raster_columns,  sql_definition: <<-SQL
-      SELECT current_database() AS r_table_catalog,
-      n.nspname AS r_table_schema,
-      c.relname AS r_table_name,
-      a.attname AS r_raster_column,
-      COALESCE(_raster_constraint_info_srid(n.nspname, c.relname, a.attname), ( SELECT st_srid('010100000000000000000000000000000000000000'::geometry) AS st_srid)) AS srid,
-      _raster_constraint_info_scale(n.nspname, c.relname, a.attname, 'x'::bpchar) AS scale_x,
-      _raster_constraint_info_scale(n.nspname, c.relname, a.attname, 'y'::bpchar) AS scale_y,
-      _raster_constraint_info_blocksize(n.nspname, c.relname, a.attname, 'width'::text) AS blocksize_x,
-      _raster_constraint_info_blocksize(n.nspname, c.relname, a.attname, 'height'::text) AS blocksize_y,
-      COALESCE(_raster_constraint_info_alignment(n.nspname, c.relname, a.attname), false) AS same_alignment,
-      COALESCE(_raster_constraint_info_regular_blocking(n.nspname, c.relname, a.attname), false) AS regular_blocking,
-      _raster_constraint_info_num_bands(n.nspname, c.relname, a.attname) AS num_bands,
-      _raster_constraint_info_pixel_types(n.nspname, c.relname, a.attname) AS pixel_types,
-      _raster_constraint_info_nodata_values(n.nspname, c.relname, a.attname) AS nodata_values,
-      _raster_constraint_info_out_db(n.nspname, c.relname, a.attname) AS out_db,
-      _raster_constraint_info_extent(n.nspname, c.relname, a.attname) AS extent,
-      COALESCE(_raster_constraint_info_index(n.nspname, c.relname, a.attname), false) AS spatial_index
-     FROM pg_class c,
-      pg_attribute a,
-      pg_type t,
-      pg_namespace n
-    WHERE ((t.typname = 'raster'::name) AND (a.attisdropped = false) AND (a.atttypid = t.oid) AND (a.attrelid = c.oid) AND (c.relnamespace = n.oid) AND ((c.relkind)::text = ANY ((ARRAY['r'::character(1), 'v'::character(1), 'm'::character(1), 'f'::character(1)])::text[])) AND (NOT pg_is_other_temp_schema(c.relnamespace)) AND has_table_privilege(c.oid, 'SELECT'::text));
-  SQL
-
-  create_view :raster_overviews,  sql_definition: <<-SQL
-      SELECT current_database() AS o_table_catalog,
-      n.nspname AS o_table_schema,
-      c.relname AS o_table_name,
-      a.attname AS o_raster_column,
-      current_database() AS r_table_catalog,
-      (split_part(split_part(s.consrc, '''::name'::text, 1), ''''::text, 2))::name AS r_table_schema,
-      (split_part(split_part(s.consrc, '''::name'::text, 2), ''''::text, 2))::name AS r_table_name,
-      (split_part(split_part(s.consrc, '''::name'::text, 3), ''''::text, 2))::name AS r_raster_column,
-      (btrim(split_part(s.consrc, ','::text, 2)))::integer AS overview_factor
-     FROM pg_class c,
-      pg_attribute a,
-      pg_type t,
-      pg_namespace n,
-      pg_constraint s
-    WHERE ((t.typname = 'raster'::name) AND (a.attisdropped = false) AND (a.atttypid = t.oid) AND (a.attrelid = c.oid) AND (c.relnamespace = n.oid) AND ((c.relkind)::text = ANY ((ARRAY['r'::character(1), 'v'::character(1), 'm'::character(1), 'f'::character(1)])::text[])) AND (s.connamespace = n.oid) AND (s.conrelid = c.oid) AND (s.consrc ~~ '%_overview_constraint(%'::text) AND (NOT pg_is_other_temp_schema(c.relnamespace)) AND has_table_privilege(c.oid, 'SELECT'::text));
-  SQL
-
   create_view :commuting_stop_event_clusters,  sql_definition: <<-SQL
       SELECT row_number() OVER () AS id,
       st_numgeometries(f.gc) AS st_numgeometries,
