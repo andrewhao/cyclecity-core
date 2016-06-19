1609dcfb71f94284a53acaae963608db54f704eb
Andrew Hao <andrewhao@gmail.com>
2016-06-19 16:44:04 -0700

    Adding commute listing.

--- spec/controllers/commuting/commutes_controller_spec.rb
+++ spec/controllers/commuting/commutes_controller_spec.rb
@@ -6,7 +6,7 @@ describe Commuting::CommutesController do
   # Commuting::Commute. As you add validations to Commuting::Commute, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    attributes_for :commute
+    { name: 'hello', started_at: Time.zone.now }
   }
 
   let(:invalid_attributes) {
@@ -40,102 +40,4 @@ describe Commuting::CommutesController do
       expect(assigns(:commuting_commute)).to be_a_new(Commuting::Commute)
     end
   end
-
-  describe "GET #edit" do
-    it "assigns the requested commuting_commute as @commuting_commute" do
-      commute = Commuting::Commute.create! valid_attributes
-      get :edit, { id: commute.to_param }, valid_session
-      expect(assigns(:commuting_commute)).to eq(commute)
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Commuting::Commute" do
-        expect {
-          post :create, {:commuting_commute => valid_attributes}, valid_session
-        }.to change(Commuting::Commute, :count).by(1)
-      end
-
-      it "assigns a newly created commuting_commute as @commuting_commute" do
-        post :create, {:commuting_commute => valid_attributes}, valid_session
-        expect(assigns(:commuting_commute)).to be_a(Commuting::Commute)
-        expect(assigns(:commuting_commute)).to be_persisted
-      end
-
-      it "redirects to the created commuting_commute" do
-        post :create, {:commuting_commute => valid_attributes}, valid_session
-        expect(response).to redirect_to(Commuting::Commute.last)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns a newly created but unsaved commuting_commute as @commuting_commute" do
-        post :create, {:commuting_commute => invalid_attributes}, valid_session
-        expect(assigns(:commuting_commute)).to be_a_new(Commuting::Commute)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, {:commuting_commute => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested commuting_commute" do
-        commute = Commuting::Commute.create! valid_attributes
-        put :update, { id: commute.to_param, :commuting_commute => new_attributes }, valid_session
-        commute.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "assigns the requested commuting_commute as @commuting_commute" do
-        commute = Commuting::Commute.create! valid_attributes
-        put :update, { id: commute.to_param, :commuting_commute => valid_attributes }, valid_session
-        expect(assigns(:commuting_commute)).to eq(commute)
-      end
-
-      it "redirects to the commuting_commute" do
-        commute = Commuting::Commute.create! valid_attributes
-        put :update, { id: commute.to_param, :commuting_commute => valid_attributes }, valid_session
-        expect(response).to redirect_to(commute)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns the commuting_commute as @commuting_commute" do
-        commute = Commuting::Commute.create! valid_attributes
-        put :update, { id: commute.to_param, :commuting_commute => invalid_attributes }, valid_session
-        expect(assigns(:commuting_commute)).to eq(commute)
-      end
-
-      it "re-renders the 'edit' template" do
-        commute = Commuting::Commute.create! valid_attributes
-        put :update, { id: commute.to_param, :commuting_commute => invalid_attributes }, valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested commuting_commute" do
-      commute = Commuting::Commute.create! valid_attributes
-      expect {
-        delete :destroy, { id: commute.to_param }, valid_session
-      }.to change(Commuting::Commute, :count).by(-1)
-    end
-
-    it "redirects to the commuting_commutes list" do
-      commute = Commuting::Commute.create! valid_attributes
-      delete :destroy, { id: commute.to_param }, valid_session
-      expect(response).to redirect_to(commuting_commutes_url)
-    end
-  end
-
 end
